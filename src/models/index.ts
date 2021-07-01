6a1182bcff23b6ad35d75071ea839d8c6eb1261c
trinhdvt <trinhvideo123@gmail.com>
2021-07-01 09:58:57 +0700

    add entry class to manage db connections

---
+++ src/models/index.ts
@@ -0,0 +1,39 @@
+import {Sequelize} from "sequelize-typescript";
+import Account from "./Account";
+import Post from "./Post";
+import BookmarkedPost from "./BookmarkedPost";
+import {Category, PostCategory} from "./Category";
+import {Comment, CommentLike} from "./Comment";
+import Follow from "./FollowRelationship";
+import Notification from "./Notification";
+import PostReport from "./PostReport";
+
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 3000,
+        idle: 10000
+    },
+    benchmark: true,
+    define: {
+        underscored: true,
+        timestamps: false
+    }
+});
+
+sequelize.addModels(
+    [Account, Post, BookmarkedPost,
+        Category, PostCategory,
+        Comment, CommentLike,
+        Follow, Notification, PostReport]
+);
+
+export default sequelize;
