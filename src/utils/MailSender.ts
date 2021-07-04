17f419d8c8bfb501004a88a3473b63b6aeae8097
trinhdvt <trinhvideo123@gmail.com>
2021-07-04 17:59:56 +0700

    add helper class for sending email

---
+++ src/utils/MailSender.ts
@@ -0,0 +1,48 @@
+import {Service} from "typedi";
+import nodemailer from "nodemailer";
+
+require('dotenv').config();
+
+@Service()
+export default class MailSender {
+    readonly mailSender = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.MAIL_USER as string,
+            pass: process.env.MAIL_PASSWORD as string
+        }
+    });
+
+    public sendCreatedAccountEmail = (email: string, rawPassword: string) => {
+        const subject = "Login with Google";
+        const text = "We have created a account for you based on your google public info \r\n"
+            + "Please use this account to login and changed your password:\r\n"
+            + `- Email: ${email}\r\n`
+            + `- Password: ${rawPassword}`;
+
+        this.sendEmail(email, subject, text);
+    };
+
+    private sendEmail = (to: string, subject: string, text: string) => {
+        let emailOptions = {
+            from: "Lang Thang Project",
+            to: to,
+            subject: subject,
+            text: text
+        }
+        this.mailSender.sendMail(emailOptions, ((err, info) => {
+            if (err)
+                console.error(err);
+        }));
+    };
+
+    public verify = () => {
+        this.mailSender.verify((err, success) => {
+            if (err)
+                console.error(err)
+            else console.log("Ready to use");
+        });
+    }
+}
