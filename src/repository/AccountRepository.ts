be307eff4b2ea835359546596738058db43cf79a
trinhdvt <trinhvideo123@gmail.com>
2021-07-04 18:02:22 +0700

    add AccountRepository
    This class provide all method to access database with Account model

---
+++ src/repository/AccountRepository.ts
@@ -0,0 +1,21 @@
+import {Service} from "typedi";
+import Account from "../models/Account";
+
+@Service()
+export default class AccountRepository {
+
+    public async findByEmail(email: string): Promise<Account | null> {
+        return await Account.findOne({
+            where: {email: email}
+        });
+    }
+
+    public async findByEmailAndEnable(email: string, enabled: boolean): Promise<Account | null> {
+        return await Account.findOne({
+            where: {
+                email: email,
+                enabled: enabled
+            }
+        });
+    }
+}
