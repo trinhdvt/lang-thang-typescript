ce19eefb026f80f69d9d45d2c4c648c0048e666a
trinhdvt <trinhvideo123@gmail.com>
2021-07-05 18:13:25 +0700

    add email validation method

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,3 +1,5 @@
+import {randomUUID} from "crypto";
+
 export default class StringUtils {
 
     public static randomString(length: number) {
@@ -6,7 +8,17 @@ export default class StringUtils {
         return arr.join('');
     }
 
+    public static isEmail(str: string): boolean {
+        let pattern = /^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$/;
+        return pattern.test(str);
+    }
+
+    public static randomUUID(): string {
+        return randomUUID({disableEntropyCache: true});
+    }
+
     private static randomInt(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
 }
